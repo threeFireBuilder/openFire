3f2e12a933a76604815b6892ece1c67954d5d90f
zhangyan <zhangyansz1@codemao.cn>
2022-07-20 12:33:21 +0800

    提交代码

---
+++ javaBase/src/main/java/base/TestBase.java
@@ -0,0 +1,38 @@
+package base;
+
+import java.util.*;
+
+/**
+ * @author zhangyan
+ * @version 1.0
+ * @date 2022/7/11 3:52 PM
+ */
+public class TestBase {
+
+    public static void main(String args[]){
+
+        Long.valueOf(0L);
+
+        int array[] = {1,2,3,4,5,6,7,8};
+        List list = Arrays.asList(array);
+        // Long count = list.stream().filter(it -> it > 7).count();
+//        String s = "1111-3333";
+//
+//        String ss[] = s.split(",");
+//        for (String temp: ss){
+//            System.out.println(temp);
+//        }
+
+        Long endTime = 1662861600L;
+//        endTime = startTime + coursePacketTimeNum / 2 * 7 * 3600;
+        String times[] = "1662739200-1662998400".split(",");
+        for (String tempTime : times) {
+            String s[] = tempTime.split("-");
+            if (endTime > Long.valueOf(s[0]) && endTime < Long.valueOf(s[1])){
+                System.out.println("add Time");
+                endTime = endTime + 7*24 *3600;
+            }
+        }
+        System.out.println(endTime);
+    }
+}
--- spring/demo-spring-boot-example-06/src/main/java/demo/DemoApplication.java
+++ spring/demo-spring-boot-example-06/src/main/java/demo/DemoApplication.java
@@ -18,50 +18,50 @@ import java.util.concurrent.TimeUnit;
 @Import(demospringbootexample06.demo.SpringContextUtil.class)
 public class DemoApplication {
 
-    private final Person person;
+//    private final Person person;
 
 
     @Resource
     private demo.DefaultProperties defaultProperties;
 
-    @Resource
-    private Redisson redisson;
-
-
-    public DemoApplication(Person person) {
-        this.person = person;
-    }
-
-    @GetMapping("/hello1")
-    public String hello1(){
-        System.out.println();
-        System.out.println(defaultProperties.getAge());
-        demo.TestServiceImpl testServiceImpl = demospringbootexample06.demo.SpringContextUtil.getBean("testServiceImpl");
-        System.out.println(testServiceImpl.test());
-        return defaultProperties.getName();
-    }
-
-
-    @GetMapping("/hello")
-    public String hello(){
-        //1、获取一把锁，只要锁的名字一样，就是同一把锁
-        RLock lock = redisson.getLock("my-lock");
-        //2、加锁 默认加锁时间30s
-        lock.lock();
-        try {
-            System.out.println("加锁成功，执行业务..."  + Thread.currentThread().getId());
-            TimeUnit.SECONDS.sleep(40);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            //3、解锁
-            System.out.println("释放锁..." + Thread.currentThread().getId());
-            lock.unlock();
-        }
-
-        return "hello";
-
-    }
+//    @Resource
+//    private Redisson redisson;
+//
+//
+//    public DemoApplication(Person person) {
+//        this.person = person;
+//    }
+//
+//    @GetMapping("/hello1")
+//    public String hello1(){
+//        System.out.println();
+//        System.out.println(defaultProperties.getAge());
+//        demo.TestServiceImpl testServiceImpl = demospringbootexample06.demo.SpringContextUtil.getBean("testServiceImpl");
+//        System.out.println(testServiceImpl.test());
+//        return defaultProperties.getName();
+//    }
+//
+//
+//    @GetMapping("/hello")
+//    public String hello(){
+//        //1、获取一把锁，只要锁的名字一样，就是同一把锁
+//        RLock lock = redisson.getLock("my-lock");
+//        //2、加锁 默认加锁时间30s
+//        lock.lock();
+//        try {
+//            System.out.println("加锁成功，执行业务..."  + Thread.currentThread().getId());
+//            TimeUnit.SECONDS.sleep(40);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            //3、解锁
+//            System.out.println("释放锁..." + Thread.currentThread().getId());
+//            lock.unlock();
+//        }
+//
+//        return "hello";
+//
+//    }
 
     @Bean
     void test(){
